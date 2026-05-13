3c83aa01906dc1465231f46d45f1145be84b9865
TimSource-it <tim@source-it.nu>
2026-05-13 21:08:00 +0200

    Create shopify_oauth.py

---
+++ shopify_connector/controllers/shopify_oauth.py
@@ -0,0 +1,38 @@
+from odoo import http
+from odoo.http import request
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ShopifyOAuthController(http.Controller):
+
+    @http.route('/shopify/callback', type='http', auth='user', website=False)
+    def shopify_callback(self, **kwargs):
+        """Verwerkt de OAuth callback van Shopify."""
+        code = kwargs.get('code')
+        shop = kwargs.get('shop')
+        state = kwargs.get('state')
+
+        if not code or not shop:
+            return request.redirect('/web#action=shopify_connector.action_shopify_config&error=missing_params')
+
+        # Zoek de config op basis van shop naam
+        shop_name = shop.replace('.myshopify.com', '')
+        config = request.env['shopify.config'].sudo().search([
+            ('shop_name', '=', shop_name)
+        ], limit=1)
+
+        if not config:
+            # Maak nieuwe config aan
+            config = request.env['shopify.config'].sudo().create({
+                'shop_name': shop_name,
+            })
+
+        # Haal access token op
+        success = config.sudo()._exchange_code_for_token(code, shop)
+
+        if success:
+            return request.redirect(f'/web#action=shopify_connector.action_shopify_config&success=1')
+        else:
+            return request.redirect(f'/web#action=shopify_connector.action_shopify_config&error=token_exchange_failed')
