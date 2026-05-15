2eca9e23340718d00139188f7e669819c271e644
TimSource-it <tim@source-it.nu>
2026-05-15 12:16:29 +0200

    Update __init__.py

--- shopify_connector/__init__.py
+++ shopify_connector/__init__.py
@@ -1,1 +1,15 @@
 from . import models, controllers
+
+
+def post_init_hook(env):
+    model = env['ir.model'].search([('model', '=', 'shopify.config')], limit=1)
+    if model:
+        env['ir.model.access'].create({
+            'name': 'shopify.config user',
+            'model_id': model.id,
+            'group_id': env.ref('base.group_user').id,
+            'perm_read': True,
+            'perm_write': True,
+            'perm_create': True,
+            'perm_unlink': True,
+        })
