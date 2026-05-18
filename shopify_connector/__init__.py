f8c7d9623953b79978de9ba5062e2942e1c56a06
TimSource-it <tim@source-it.nu>
2026-05-18 12:24:53 +0200

    Update __init__.py

--- shopify_connector/__init__.py
+++ shopify_connector/__init__.py
@@ -2,6 +2,7 @@ from . import models, controllers
 
 
 def post_init_hook(env):
+    # Toegangsrechten aanmaken
     model = env['ir.model'].search([('model', '=', 'shopify.config')], limit=1)
     if model:
         env['ir.model.access'].create({
@@ -13,3 +14,10 @@ def post_init_hook(env):
             'perm_create': True,
             'perm_unlink': True,
         })
+    
+    # Views en menu laden
+    import os
+    from odoo.tools import convert_file
+    module_path = os.path.dirname(__file__)
+    for xml_file in ['views/shopify_config_views.xml', 'data/shopify_cron.xml']:
+        convert_file(env, 'shopify_connector', xml_file, {}, mode='init', noupdate=False)
